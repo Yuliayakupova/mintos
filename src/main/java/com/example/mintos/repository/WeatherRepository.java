96d08417a172ac715123cfa7007dcc3082aa305d
yulia.yakupova <yulia.yakupova@accenture.com>
2023-05-26 18:37:28 +0300

    Another fix insert into the table WeatherData

--- src/main/java/com/example/mintos/repository/WeatherRepository.java
+++ src/main/java/com/example/mintos/repository/WeatherRepository.java
@@ -3,13 +3,34 @@ package com.example.mintos.repository;
 import com.example.mintos.dto.WeatherDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
-import java.awt.*;
 
 @Repository
 public class WeatherRepository {
+
+    private final static String INTO_WEATHER_DATA = """
+        INSERT INTO 
+            WeatherData (
+                latitude, 
+                longitude, 
+                generationtime_ms, 
+                utc_offset_seconds, 
+                timezone, 
+                timezone_abbreviation, 
+                elevation, 
+                hourly_units_temperature_2m, 
+                hourly_units_time
+        ) VALUES (
+            ?, 
+            ?, 
+            ?, 
+            ?, 
+            ?, 
+            ?, 
+            ?, 
+            ?, 
+            ?)
+        """;
 
     @Autowired
     private final JdbcTemplate connection;
@@ -19,25 +40,29 @@ public class WeatherRepository {
     }
 
     public void saveWeatherData(WeatherDTO weatherData) {
+
         try{
-            String insertQuery = "INSERT INTO WeatherData (latitude, longitude, generationtime_ms, utc_offset_seconds, timezone, )" +
-                                  "timezone_abbreviation, elevation, hourly_units_temperature_2m, hourly_units_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            WeatherDTO.HourlyData hourly =  weatherData.getHourly();
 
-            double[] temperatures = weatherData.getHourly().getTemperature_2m().stream().mapToDouble(Double::doubleValue).toArray();
-            String[] times = weatherData.getHourly().getTime().toArray(new String[0]);
+            final Double temperature = hourly.getTemperature_2m().isEmpty()
+                    ? null
+                    : hourly.getTemperature_2m().get(0);
 
-            for (int i = 0; i < Math.min(temperatures.length, times.length); i++) {
-                connection.update(insertQuery,
-                        weatherData.getLatitude(),
-                        weatherData.getLongitude(),
-                        weatherData.getGenerationtime_ms(),
-                        weatherData.getUtc_offset_seconds(),
-                        weatherData.getTimezone(),
-                        weatherData.getTimezone_abbreviation(),
-                        weatherData.getElevation(),
-                        temperatures[i],
-                        times[i]);
-            }
+            final String time = hourly.getTime().isEmpty()
+                    ? null
+                    : hourly.getTime().get(0);
+
+            connection.update(INTO_WEATHER_DATA,
+                    weatherData.getLatitude(),
+                    weatherData.getLongitude(),
+                    weatherData.getGenerationtime_ms(),
+                    weatherData.getUtc_offset_seconds(),
+                    weatherData.getTimezone(),
+                    weatherData.getTimezone_abbreviation(),
+                    weatherData.getElevation(),
+                    temperature,
+                    time);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
